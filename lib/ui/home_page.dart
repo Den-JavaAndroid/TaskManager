4efed08f9ade5a0d795153ca8aa3cbacbef650a0
Denis <jaddev888@gmail.com>
2022-09-16 14:32:36 +0300

    add datePicker widget

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,6 +1,8 @@
+import 'package:date_picker_timeline/date_picker_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import 'package:task_manager/services/notificaton_services.dart';
 import 'package:task_manager/services/theme_services.dart';
@@ -16,6 +18,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var notifyHelper;
+  DateTime _selectedDate = DateTime.now();
 
   @override
   void initState() {
@@ -32,6 +35,41 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: [
           _addTaskBar(),
+          Container(
+            margin: const EdgeInsets.only(top: 20, left: 20),
+            child: DatePicker(
+              DateTime.now(),
+              height: 100,
+              width: 80,
+              initialSelectedDate: DateTime.now(),
+              selectionColor: primaryClr,
+              selectedTextColor: Colors.white,
+              dateTextStyle: GoogleFonts.lato(
+                  textStyle: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey
+                  )
+              ),
+              dayTextStyle: GoogleFonts.lato(
+                  textStyle: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey
+                  )
+              ),
+              monthTextStyle: GoogleFonts.lato(
+                  textStyle: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.grey
+                  )
+              ),
+              onDateChange: (date){
+                _selectedDate = date;
+              },
+            ),
+          )
         ],
       ),
     );
