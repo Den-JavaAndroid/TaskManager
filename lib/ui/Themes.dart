afe3ac8ffac99adef37c048f00d0e0cf00f99d9d
Denis <jaddev888@gmail.com>
2022-09-16 13:41:06 +0300

    add task bar and improve theme changing icons

--- lib/ui/Themes.dart
+++ lib/ui/Themes.dart
@@ -1,19 +1,20 @@
 import 'dart:ui';
-
+import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-const Map<int, Color> color =
-{
-  50:Color.fromRGBO(136,14,79, .1),
-  100:Color.fromRGBO(136,14,79, .2),
-  200:Color.fromRGBO(136,14,79, .3),
-  300:Color.fromRGBO(136,14,79, .4),
-  400:Color.fromRGBO(136,14,79, .5),
-  500:Color.fromRGBO(136,14,79, .6),
-  600:Color.fromRGBO(136,14,79, .7),
-  700:Color.fromRGBO(136,14,79, .8),
-  800:Color.fromRGBO(136,14,79, .9),
-  900:Color.fromRGBO(136,14,79, 1),
+const Map<int, Color> color = {
+  50: Color.fromRGBO(136, 14, 79, .1),
+  100: Color.fromRGBO(136, 14, 79, .2),
+  200: Color.fromRGBO(136, 14, 79, .3),
+  300: Color.fromRGBO(136, 14, 79, .4),
+  400: Color.fromRGBO(136, 14, 79, .5),
+  500: Color.fromRGBO(136, 14, 79, .6),
+  600: Color.fromRGBO(136, 14, 79, .7),
+  700: Color.fromRGBO(136, 14, 79, .8),
+  800: Color.fromRGBO(136, 14, 79, .9),
+  900: Color.fromRGBO(136, 14, 79, 1),
 };
 
 const Color bluishClr = Color(0xFF4e5ae8);
@@ -23,9 +24,33 @@ const Color white = Colors.white;
 const primaryClr = bluishClr;
 const Color darkGreyClr = Color(0xFF121212);
 const darkHeaderClr = Color(0xFF424242);
+
 class Themes {
-  static final light =
-      ThemeData(primarySwatch: MaterialColor(primaryClr.value, color), brightness: Brightness.light);
-  static final dark =
-      ThemeData(primarySwatch: MaterialColor(darkGreyClr.value, color), brightness: Brightness.dark);
+  static final light = ThemeData(
+      backgroundColor: Colors.white,
+      primarySwatch: MaterialColor(primaryClr.value, color),
+      brightness: Brightness.light);
+  static final dark = ThemeData(
+      backgroundColor: darkGreyClr,
+      primarySwatch: MaterialColor(darkGreyClr.value, color),
+      brightness: Brightness.dark);
+}
+
+TextStyle get subHeadingStyle{
+  return GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+      color: Get.isDarkMode?Colors.grey[400]:Colors.grey
+    )
+  );
+}
+TextStyle get headingStyle{
+  return GoogleFonts.lato(
+      textStyle: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          color: Get.isDarkMode?Colors.white:Colors.black
+      )
+  );
 }
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 import 'package:task_manager/services/notificaton_services.dart';
 import 'package:task_manager/services/theme_services.dart';
+import 'package:task_manager/ui/Themes.dart';
+import 'package:task_manager/ui/widgets/button.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -27,11 +30,35 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: _appBar(),
       body: Column(
-        children: const [
-          Text("Theme Data",
-            style: TextStyle(
-                fontSize: 30
-            ),)
+        children: [
+          _addTaskBar(),
+        ],
+      ),
+    );
+  }
+
+  _addTaskBar(){
+    return           Container(
+      margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  DateFormat.yMMMMd().format(DateTime.now()),
+                  style: subHeadingStyle,
+                ),
+                Text(
+                  "Today",
+                  style: headingStyle,
+                )
+              ],
+            ),
+          ),
+          MyButton(label: "+ Add Task", onTap: ()=>null)
         ],
       ),
     );
@@ -39,20 +66,30 @@ class _HomePageState extends State<HomePage> {
 
   _appBar() {
     return AppBar(
+      elevation: 0,
+      backgroundColor: context.theme.backgroundColor,
       leading: GestureDetector(
         onTap: () {
           ThemeServices().switchTheme();
           notifyHelper.displayNotification(
               title: "Theme changed",
-              body: !Get.isDarkMode?"Activated Dark Theme":"Activated Light Theme"
-          );
-          notifyHelper.scheduledNotification();
+              body: !Get.isDarkMode
+                  ? "Activated Dark Theme"
+                  : "Activated Light Theme");
+          // notifyHelper.scheduledNotification();
         },
-        child: const Icon(Icons.nightlight_round, size: 20,),
+        child: Icon(
+            Get.isDarkMode ? Icons.wb_sunny_outlined : Icons.nightlight_round,
+            size: 20,
+            color: Get.isDarkMode ? Colors.white : Colors.black),
       ),
       actions: const [
-        Icon(Icons.person, size: 20,),
-        SizedBox(width: 20,)
+        CircleAvatar(
+          backgroundImage: AssetImage("images/profile.png"),
+        ),
+        SizedBox(
+          width: 20,
+        )
       ],
     );
   }
